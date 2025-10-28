19aef8ec47e4e3a5cc177522560ffbfec7ab8896
Jeremy Le Nezet <jle-neze@f0r8s1.clusters.42paris.fr>
2025-10-28 16:34:37 +0100

    push before raycast and draw

--- srcs/exec/loop.c
+++ srcs/exec/loop.c
@@ -14,12 +14,9 @@
 
 int	game_loop(t_game *g)
 {
-	/* 1) Update joueur selon input */
 	player_update(g);
-	/* 2) Dessin */
 	draw_background(g);
 	raycast_frame(g);
 	mlx_put_image_to_window(g->gfx.mlx, g->gfx.win, g->gfx.frame.ptr, 0, 0);
 	return (0);
 }
-/* Note: on pourrait optimiser en ne redessinant que ce qui change */
